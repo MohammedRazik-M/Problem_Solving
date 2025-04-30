c12c39698b158b8e343f6e816ba1bc596955f670
Mohammed Razik M <150541743+MohammedRazik-M@users.noreply.github.com>
2025-05-01 00:05:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Multiply two strings/multiply-two-strings.java
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.java
@@ -20,58 +20,57 @@ class Multiply {
 }
 // } Driver Code Ends
 
+
+// User function Template for Java
+
 class Solution {
     public String multiplyStrings(String s1, String s2) {
         boolean negative = false;
         if(s1.charAt(0) == '-' && s2.charAt(0) != '-') {
             negative = true;
             s1 = s1.substring(1);
-        } 
+        }
         else if(s2.charAt(0) == '-' && s1.charAt(0) != '-') {
             negative = true;
             s2 = s2.substring(1);
-        } 
-        else if(s1.charAt(0) == '-' && s2.charAt(0) == '-') {
+        }
+        else if(s1.charAt(0) == '-' && s2.charAt(0) == '-'){
             s1 = s1.substring(1);
             s2 = s2.substring(1);
         }
         
-        if (isZero(s1) || isZero(s2)) {
-            return "0";
-        }
+        if(isZero(s1) || isZero(s2)) return "0";
         
         int n = s1.length();
         int m = s2.length();
-        int[] result = new int[n + m];
         
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = m - 1; j >= 0; j--) {
+        int[] res = new int[n+m];
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int j=m-1; j>=0; j--) {
                 int mul = (s1.charAt(i) - '0') * (s2.charAt(j) - '0');
-                int sum = mul + result[i + j + 1];
-                result[i + j + 1] = sum % 10;
-                result[i + j] += sum / 10;
+                int sum = mul + res[i+j+1];
+                res[i+j+1] = sum%10;
+                res[i+j] += sum/10;
             }
         }
         
-        StringBuilder sb = new StringBuilder();
         int i = 0;
-        while(i < result.length && result[i] == 0) {
+        StringBuilder sb = new StringBuilder();
+        while(i < res.length && res[i] == 0) {
             i++;
         }
-        for(; i < result.length; i++) {
-            sb.append(result[i]);
+        for(; i < res.length; i++) {
+            sb.append(res[i]);
         }
-        if(negative) {
-            sb.insert(0, '-');
-        }
+        if(negative) sb.insert(0, "-");
         return sb.toString();
     }
     private boolean isZero(String s) {
-        for(char c : s.toCharArray()) {
-            if(c != '0') {
-                return false;
-            }
+        for(char ch : s.toCharArray()) {
+            if(ch != '0') return false;
         }
         return true;
     }
 }
+
