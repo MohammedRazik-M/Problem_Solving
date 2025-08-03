190b5b4d11b6fe1feeb1dccb922507f70e0b15f8
Mohammed Razik M <150541743+MohammedRazik-M@users.noreply.github.com>
2025-08-03 15:16:34 +0530

    Time: 1 ms (72.4%), Space: 45.6 MB (66.66%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    private void setRow(int row, int n, int[][] matrix) {
+        for(int col=0; col<n; col++) {
+            matrix[row][col] = 0;
+        }
+    }
+
+    private void setCol(int col, int m, int[][] matrix) {
+        for(int row=0; row<m; row++) {
+            matrix[row][col] = 0;
+        }
+    }
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] rflag = new boolean[m];
+        boolean[] cflag = new boolean[n];
+
+        for(int row=0; row<m; row++) {
+            for(int col=0; col<n; col++) {
+                if(matrix[row][col] == 0) {
+                    rflag[row] = true;
+                    cflag[col] = true;
+                }
+            }
+        }
+
+        // rflag
+        for(int row=0; row<m; row++) {
+            if(rflag[row]) {
+                setRow(row, n, matrix);
+            }
+        }
+
+        // cflag
+        for(int col=0; col<n; col++) {
+            if(cflag[col]) {
+                setCol(col, m, matrix);
+            }
+        }
+    }
+}
